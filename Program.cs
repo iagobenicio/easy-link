cb0876e567e287f61d40d10951e251e390292a5a
Iago <iagobensen@gmail.com>
2023-04-29 16:18:35 -0300

    configure automapper

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
 using easy_link.Context;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using easy_link.DTOs;
+using easy_link.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var config = new MapperConfiguration(cfg => 
+{
+    cfg.CreateMap<UserDTO,User>();
+});
+IMapper mapper = config.CreateMapper();
+
 // Add services to the container.
 builder.Services.AddDbContext<EasylinkContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSingleton(mapper);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
