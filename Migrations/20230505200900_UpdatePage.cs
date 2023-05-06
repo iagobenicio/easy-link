64198e719c0ef898a4faced21faf8cb8d54beb23
Iago <iagobensen@gmail.com>
2023-05-06 20:34:12 -0300

    add imageprofile on page

---
+++ Migrations/20230505200900_UpdatePage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace easy_link.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageProfile",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageProfile",
+                table: "page",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageProfile",
+                table: "page");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageProfile",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
